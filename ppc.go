82e786b62338ef29af16028eb55348990f0d9e83
mably <f.mably@gmail.com>
2014-12-15 00:33:45 +0100

    ppc: getnextrequiredtarget command implemented

--- ppc.go
+++ ppc.go
@@ -108,3 +108,93 @@ func (c *Client) GetKernelStakeModifierVerboseAsync(blockHash *btcwire.ShaHash) 
 func (c *Client) GetKernelStakeModifierVerbose(blockHash *btcwire.ShaHash) (*btcjson.KernelStakeModifierResult, error) {
 	return c.GetKernelStakeModifierVerboseAsync(blockHash).Receive()
 }
+
+// FutureNextRequiredTargetResult is a future promise to deliver the result of a
+// GetNextRequiredTargetAsync RPC invocation (or an applicable error).
+type FutureNextRequiredTargetResult chan *response
+
+// Receive waits for the response promised by the future and returns the raw
+// block requested from the server given its hash.
+func (r FutureNextRequiredTargetResult) Receive() (uint32, error) {
+	res, err := receiveFuture(r)
+	if err != nil {
+		return 0, err
+	}
+
+	// Unmarshal the result as an uint32.
+	var nextRequiredTarget uint32
+	err = json.Unmarshal(res, &nextRequiredTarget)
+	if err != nil {
+		return 0, err
+	}
+	return nextRequiredTarget, nil
+}
+
+// GetNextRequiredTargetAsync returns an instance of a type that can be used to get the
+// result of the RPC at some future time by invoking the Receive function on the
+// returned instance.
+//
+// See GetNextRequiredTarget for the blocking version and more details.
+func (c *Client) GetNextRequiredTargetAsync(proofOfStake bool) FutureNextRequiredTargetResult {
+
+	id := c.NextID()
+	cmd, err := btcjson.NewGetNextRequiredTargetCmd(id, proofOfStake, false)
+	if err != nil {
+		return newFutureError(err)
+	}
+
+	return c.sendCmd(cmd)
+}
+
+// GetNextRequiredTarget returns a raw block from the server given its hash.
+//
+// See GetNextRequiredTargetVerbose to retrieve a data structure with information about the
+// block instead.
+func (c *Client) GetNextRequiredTarget(proofOfStake bool) (uint32, error) {
+	return c.GetNextRequiredTargetAsync(proofOfStake).Receive()
+}
+
+// FutureGetNextRequiredTargetVerboseResult is a future promise to deliver the result of a
+// GetNextRequiredTargetVerboseAsync RPC invocation (or an applicable error).
+type FutureGetNextRequiredTargetVerboseResult chan *response
+
+// Receive waits for the response promised by the future and returns the data
+// structure from the server with information about the requested block.
+func (r FutureGetNextRequiredTargetVerboseResult) Receive() (*btcjson.NextRequiredTargetResult, error) {
+	res, err := receiveFuture(r)
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal the raw result into a NextRequiredTargetResult.
+	var ksmResult btcjson.NextRequiredTargetResult
+	err = json.Unmarshal(res, &ksmResult)
+	if err != nil {
+		return nil, err
+	}
+	return &ksmResult, nil
+}
+
+// GetNextRequiredTargetVerboseAsync returns an instance of a type that can be used to get
+// the result of the RPC at some future time by invoking the Receive function on
+// the returned instance.
+//
+// See GetNextRequiredTargetVerbose for the blocking version and more details.
+func (c *Client) GetNextRequiredTargetVerboseAsync(proofOfStake bool) FutureGetNextRequiredTargetVerboseResult {
+
+	id := c.NextID()
+	cmd, err := btcjson.NewGetNextRequiredTargetCmd(id, proofOfStake, true)
+	if err != nil {
+		return newFutureError(err)
+	}
+
+	return c.sendCmd(cmd)
+}
+
+// GetNextRequiredTargetVerbose returns a data structure from the server with information
+// about a block given its hash.
+//
+// See GetNextRequiredTarget to retrieve a raw block instead.
+func (c *Client) GetNextRequiredTargetVerbose(proofOfStake bool) (*btcjson.NextRequiredTargetResult, error) {
+	return c.GetNextRequiredTargetVerboseAsync(proofOfStake).Receive()
+}
