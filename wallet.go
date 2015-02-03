4b01175735f8c7c77a882d45462acda92697e909
mably <mably>
2015-02-03 18:02:23 +0100

    Fixes for btcsuite reorg and merge

--- wallet.go
+++ wallet.go
@@ -8,11 +8,11 @@ import (
 	"encoding/json"
 	"strconv"
 
-	"github.com/btcsuite/btcjson"
-	"github.com/btcsuite/btcnet"
-	"github.com/btcsuite/btcutil"
-	"github.com/btcsuite/btcwire"
-	"github.com/btcsuite/btcws"
+	"github.com/mably/btcjson"
+	"github.com/mably/btcnet"
+	"github.com/mably/btcutil"
+	"github.com/mably/btcwire"
+	"github.com/mably/btcws"
 )
 
 // *****************************
